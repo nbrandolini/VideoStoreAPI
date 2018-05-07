ee40590dcf78b343dd13d4d8a8165d27035d9a95
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-05-07 16:12:43 -0700

    added customers and movies controllers

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,22 @@
+class CustomersController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+
+  def new
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,28 @@
 Rails.application.routes.draw do
+  get 'customers/index'
+
+  get 'customers/show'
+
+  get 'customers/update'
+
+  get 'customers/create'
+
+  get 'customers/destroy'
+
+  get 'customers/new'
+
+  get 'customers/edit'
+
+  get 'movies/index'
+
+  get 'movies/show'
+
+  get 'movies/update'
+
+  get 'movies/create'
+
+  get 'movies/destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/zomg', to: 'pets#zomg', as: "zomg"
 end
