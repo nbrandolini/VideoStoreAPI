c70e150c52355a454164d31dabe1f2d0bc370eb3
Steffany Brown <steffannbrown@gmail.com>
2018-05-07 15:43:04 -0700

    added registered_at column to customers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507223208) do
+ActiveRecord::Schema.define(version: 20180507223901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20180507223208) do
     t.string "phone"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "registered_at"
   end
 
   create_table "movies", force: :cascade do |t|
