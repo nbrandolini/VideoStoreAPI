408ad300689b6d2459e866db7307294a16e31ebe
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-05-08 10:49:33 -0700

    added tests for index moviecontroller

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,29 +1,58 @@
 require "test_helper"
 
 describe MoviesController do
-  it "should get index" do
-    get movies_index_url
-    value(response).must_be :success?
+  describe "index" do
+    it "is a real working route" do
+      get movies_path
+      must_respond_with :success
+    end
+
+    it "returns json" do
+      get movies_path
+      response.header['Content-Type'].must_include 'json'
+    end
+
+    it "returns an Array" do
+      get movies_path
+      body = JSON.parse(response.body)
+      body.must_be_instance_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_path
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with exactly the fields required" do
+      keys = %w(id title release_date)
+      get movies_path
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys.sort
+      end
+    end
   end
 
-  it "should get show" do
-    get movies_show_url
-    value(response).must_be :success?
-  end
 
-  it "should get update" do
-    get movies_update_url
-    value(response).must_be :success?
-  end
-
-  it "should get create" do
-    get movies_create_url
-    value(response).must_be :success?
-  end
-
-  it "should get destroy" do
-    get movies_destroy_url
-    value(response).must_be :success?
-  end
+  # it "should get show" do
+  #   get movies_show_url
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "should get update" do
+  #   get movies_update_url
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "should get create" do
+  #   get movies_create_url
+  #   value(response).must_be :success?
+  # end
+  #
+  # it "should get destroy" do
+  #   get movies_destroy_url
+  #   value(response).must_be :success?
+  # end
 
 end
