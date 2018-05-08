754b96124f1da1843463f5f01163d52862948988
Steffany Brown <steffannbrown@gmail.com>
2018-05-08 11:04:09 -0700

    updated model validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
 class Customer < ApplicationRecord
-  validates :name, presence: :true, format: { with: /\A[a-zA-Z]+\z/,
+  validates :name, presence: :true, format: { with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/,
     message: "Only allows letters" }
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
   validates :release_date, presence: true
-  validates :inventory, presence: true, numericality: { only_integer: true, greater_than: 0 }
+  validates :inventory, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
