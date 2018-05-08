a677b530adec62db3a2d28b10c04df2c3d9d0a1c
Steffany Brown <steffannbrown@gmail.com>
2018-05-08 12:53:26 -0700

    basic model tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,12 +1,17 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:customer) { Customer.new(name: "Captain Planet", registered_at: "Wed, 29 Apr 2015 07:54:14 -0700", address: "123 Green St", city: "Tree Town", state: "HI", postal_code: "12345", phone: "(919) 434-3203") }
 
   it "must be valid" do
     value(customer).must_be :valid?
   end
 
   it "must not save if there is no name" do
+    proc { Customer.new(address: "1234 Candy Cane Lane") }.must_change 'Customer.count', 0
+  end
+
+  it "must not save if name data is invalid" do
+    proc { Customer.new(name: "1234") }.must_change 'Customer.count', 0
   end
 end
--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,21 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let(:movie) { Movie.new(title: "Dogtoth", overview: "Weird and good", release_date: "1979-01-18", inventory: 1)}
 
   it "must be valid" do
     value(movie).must_be :valid?
+  end
+
+  it "must not create without title" do
+    proc { Movie.new(release_date: "2015-12-2", inventory: 2) }.must_change 'Movie.count', 0
+  end
+
+  it "must not create with invalid title data" do
+    proc { Movie.new(title: "123", release_date: "2015-12-2", inventory: 2) }.must_change 'Movie.count', 0
+  end
+
+  it "must not create without release date" do
+    proc { Movie.new(title: "Party", inventory: 2) }.must_change 'Movie.count', 0
   end
 end
