6ded50d35aac3e889a4e026c51d36ddf3005a73c
nicoletabrandolini <nicoletabrandolini@gmail.com>
2018-05-08 20:39:29 -0700

    added routes to rental controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
 
-  get 'rentals/check_in'
-
-  get 'rentals/check_out'
-
   resources :customers, only: [:index]
 
   resources :movies, only: [:index, :show, :create]
 
+  post 'rentals/check_out', to: 'rentals#check_out', as: 'check_out'
+
+  post 'rentals/check_in', to: 'rentals#check_in', as: 'check_in'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
